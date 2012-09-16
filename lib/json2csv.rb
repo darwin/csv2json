60522df79f3730ab0cf537c66f0291a09f84339c
rory <rgibson@technophobia.com>
2012-09-16 10:40:38 +0100

    Sorted CSV output from json2csv if you supply a -H option

--- lib/json2csv.rb
+++ lib/json2csv.rb
@@ -5,28 +5,32 @@ require 'csv2json-version.rb'
 
 module JSON2CSV
 
-    # convert an input string value to integer or float if applicable
-    def convert(val)
-        return Integer(val) if val.to_i.to_s == val
-        Float(val) rescue val
-    end
+    def parse(input, output, headers=nil, csvOptions=nil)
+        json = JSON.parse(input)
 
+        unless headers
+            headers = json[0].keys
+        end
 
-    def parse(input, output, headers=nil, options={})
-        inputJSON = JSON.parse(input)
+        outputCSV = FasterCSV.generate(csvOptions) do |csv|
+            csv << headers
 
-        outputCSV = FasterCSV.generate do |csv|
-            csv << inputJSON[0].keys
-            inputJSON.each do |row|
-                csv << row.values
+            json.each do |obj|
+                sortedValues = Array.new(headers.length)
+
+                obj.each do |key, val|
+                    headers.each_index { |i|
+                        sortedValues[i] = val if headers[i] == key
+                    }
+                end
+
+                csv << sortedValues
             end
         end
         
         output << outputCSV
     end
 
-    
     module_function :parse
-    module_function :convert
     
 end
--- test/test_json2csv.rb
+++ test/test_json2csv.rb
@@ -1,20 +1,23 @@
 require 'helper'
 require File.join(File.expand_path(File.dirname(__FILE__)), '..', 'lib', 'csv2json.rb') # this form is important for local development
 
-class TestCsv2json < Test::Unit::TestCase
+class TestJson2Csv < Test::Unit::TestCase
     SEPS = {:comma => ',', :pipe => '|', :semicolon => ';'}
+
     should "parse some test files" do
         fixtures_dir = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))
         Dir.chdir(fixtures_dir) do
-            Dir.glob('*.csv') do |filename|
-                filename_parts = File.basename(filename, ".csv").split('_')
-                json_template = filename_parts[0] + '.json'
-                File.open(filename, "r") do |input|
+            Dir.glob('*.json') do |filename|
+                filename = File.basename(filename, ".json")
+
+                SEPS.each do |sep|
+                    input = File.read(filename)
                     output = StringIO.new()
-                    CSV2JSON.parse(input, output, nil, :col_sep => SEPS[filename_parts[1].to_sym] )
-                    template = File.read(json_template)
+                    JSON2CSV.parse(input, output, nil, {:col_sep => sep.to_sym} )
                     output.pos = 0
-                    assert template == output.read
+
+                    fileToCompareTo = File.read("#{filename}_#{sep}.csv")
+                    assert output.read == fileToCompareTo
                 end
             end
         end
